32c5f851d4e8302fd764ea565c63f030426b2ce1
izan.santana <izan.santana@tadaima.studio>
2020-11-28 11:15:50 +0100

    [refactor] Added abstract bound class

---
+++ src/main/java/usantatecla/Bound.java
@@ -0,0 +1,44 @@
+package usantatecla;
+
+public abstract class Bound {
+
+	protected double value;
+
+	public Bound(double value) {
+		this.value = value;
+	}
+
+	public abstract boolean isWithin(double value);
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(value);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Min other = (Min) obj;
+		if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public abstract String toString();
+
+}
